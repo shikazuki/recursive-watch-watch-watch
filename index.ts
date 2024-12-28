5f96fe2bac96b409f9024fc9389d0366af83658a
shikazuki <shikazuki0@gmail.com>
2024-12-28 23:42:10 +0900

    リファクタリング

--- index.ts
+++ index.ts
@@ -11,55 +11,77 @@ function originalStream() {
   ending();
 }
 
-interface Comedian {
-  name: string;
-  shouldWatch: boolean;
+class Comedian {
+  constructor(private name: string, private shouldWatch: boolean, public isFirst: boolean = false) {}
+
+  private _getOriginalStreamDescription(): string {
+    return "川原さんのYoutube";
+  }
+
+  private _getSelfDescription(): string {
+    return `僕が${this.shouldWatch ? "見てる" : "見てない"}`;
+  }
+
+  private _getOtherDescription(comedian: Comedian): string {
+    return `${comedian.name}さんが${comedian.shouldWatch ? "見てる" : "見てない"}`;
+  }
+
+  private _getDescription(comedians: Comedian[]): string {
+    return comedians.map(c => {
+      // 本当はequalsメソッド的なのがあった方が良い
+      if (c === this) {
+        return `${this._getSelfDescription()}のを`;
+      } else {
+        return `${this._getOtherDescription(c)}のを`;
+      }
+    }).join("");
+  }
+
+  talkOpening(comedians: Comedian[]) {
+    const description = this._getDescription(comedians);
+    const opening = `${this._getOriginalStreamDescription()}を${description}皆さんに見てもらいます。`;
+    console.log(opening);
+  }
+
+  talkEnding(comedians: Comedian[]) {
+    const description = this._getDescription(comedians);
+    const ending = `${this._getOriginalStreamDescription()}を${description}皆さんに見てもらいました。ありがとうございました。`;
+    console.log(ending);
+  }
 }
 
-
-function getDescription(comedians: Comedian[]): string {
-  return ["川原さんのYoutubeを"]
-    .concat(comedians.map(comedian => `${comedian.name}さんが${comedian.shouldWatch ? "見てる" : "見てない"}のを`))
-    .join("");
-}
-
-function recursive_watch(comedians: Comedian[], isFirst: boolean) {
-  // 探索要素が無くなった場合の処理。
+function recursive_watch(comedians: Comedian[]) {
   if (comedians.length === 0) {
     // 最後まで行ったら元々のYoutubeが流れる。
     originalStream();
     return;
   }
 
-  if (isFirst) {
+  const currentComedian = comedians[comedians.length - 1];
+  if (currentComedian.isFirst) {
     // 動画の最初の人だけオープニングが流れる
     opening();
   }
-  const preDescription = `${getDescription(comedians)}皆さんに見てもらいます。`;
-  console.log(preDescription);
-
-  // 自身を除いた配列にする。実行が終わった要素を除いていくことで処理が終了に向かう
-  const previousComedians = comedians.slice(0, -1);
+  currentComedian.talkOpening(comedians);
   // PCを開いて動画を見る。
-  recursive_watch(previousComedians, false);
-  const postDescription = `${getDescription(comedians)}皆さんに見てもらいました。ありがとうございました。`;
-  console.log(postDescription);
+  recursive_watch(comedians.slice(0, -1));
+  currentComedian.talkEnding(comedians);
   ending();
 }
 
 function main() {
-  const comedians: Comedian[] = [
-    { name: "川原", shouldWatch: true },
-    { name: "秋山", shouldWatch: true },
-    { name: "山名", shouldWatch: true },
-    { name: "川西", shouldWatch: true },
-    { name: "石井", shouldWatch: true },
-    { name: "津田", shouldWatch: false },
-    { name: "川原", shouldWatch: false },
-    { name: "秋山", shouldWatch: true },
-  ];
   const isFirst = true;
-  recursive_watch(comedians, isFirst);
+  const comedians: Comedian[] = [
+    new Comedian("川原", true),
+    new Comedian("秋山", true),
+    new Comedian("山名", true),
+    new Comedian("川西", true),
+    new Comedian("石井", true),
+    new Comedian("津田", false),
+    new Comedian("川原", false),
+    new Comedian("秋山", true, isFirst),
+  ];
+  recursive_watch(comedians);
 }
 
 main();
